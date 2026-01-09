ca1c6db5fda3783306b1dd5293d71e8de8fb1149
♡ Chloe ༘˚⋆𐙚｡⋆𖦹.✧˚ <s2025048@mcs.edu.hk>
2026-01-09 22:28:11 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(0)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showNumber(1)
+    } else {
+        basic.showNumber(2)
+    }
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 radio.setGroup(29)
-if (input.buttonIsPressed(Button.A)) {
-    basic.showNumber(0)
-} else if (input.buttonIsPressed(Button.B)) {
-    basic.showNumber(1)
-    music.play(music.stringPlayable("C D E F G A B C5 ", 321), music.PlaybackMode.UntilDone)
-    basic.showIcon(IconNames.Yes)
-} else {
-    basic.showNumber(2)
-    music.play(music.stringPlayable("C5 B A G F E D C ", 321), music.PlaybackMode.UntilDone)
-    basic.showIcon(IconNames.No)
-}
